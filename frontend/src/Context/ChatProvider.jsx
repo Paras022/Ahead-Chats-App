6bb64cc918d4079f617085cdedd4e756e18884d9
PARAS SHARMA <parassharma0220@gmail.com>
2025-05-08 20:10:24 +0530

    Update ChatProvider.jsx

--- frontend/src/Context/ChatProvider.jsx
+++ frontend/src/Context/ChatProvider.jsx
@@ -6,7 +6,7 @@ const ChatContext = createContext()
 
 const ChatProvider = ({children}) =>{
 
-  const url = "http://localhost:8000;
+  const url = "http://localhost:8000";
    const [user , setUser ] =  useState();
    const [selectedChat , setSelectedChat] = useState();
    const [chats ,setChats] = useState([]);
